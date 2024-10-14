de4d7af04c20ea89299bec82562762e927c7d9bf
Lipun <lipundani@gmail.com>
2024-10-14 21:37:44 +0530

    transactional: added transactional annotation to ensure data consistency

--- src/main/java/com/example/Attyre/Assignment/Service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/Attyre/Assignment/Service/Impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -22,6 +23,7 @@ public class UserServiceImpl implements UserService {
     private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
+    @Transactional
     public User saveUser(UserDTO userDTO) {
         try {
             logger.info("Creating user");
