c8f813959d1bbd0208cc98bbfa0058bb608b8633
Lipun <lipundani@gmail.com>
2024-10-14 16:36:17 +0530

    user service: added feat to save new user

---
+++ src/main/java/com/example/Attyre/Assignment/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.Attyre.Assignment.Service.Impl;
+
+import com.example.Attyre.Assignment.DTO.UserDTO;
+import com.example.Attyre.Assignment.Entity.User;
+import com.example.Attyre.Assignment.Exception.InternalServerException;
+import com.example.Attyre.Assignment.Repository.UserRepo;
+import com.example.Attyre.Assignment.Service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepo userRepo;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Override
+    public User saveUser(UserDTO userDTO) {
+        try {
+            logger.info("Creating user");
+            User user = User.builder()
+                    .username(userDTO.getUsername())
+                    .display_name(userDTO.getDisplay_name())
+                    .bio(userDTO.getBio())
+                    .build();
+
+            User newUser = userRepo.save(user);
+            logger.info("User Successfully created for id: {}", newUser.getId());
+            return newUser;
+        }catch (Exception exception){
+            logger.info("Exception occurred during saving user");
+            throw new InternalServerException(exception);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/example/Attyre/Assignment/Service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.Attyre.Assignment.Service;
+
+import com.example.Attyre.Assignment.DTO.UserDTO;
+import com.example.Attyre.Assignment.Entity.User;
+
+public interface UserService {
+    public User saveUser(UserDTO userDTO);
+}
