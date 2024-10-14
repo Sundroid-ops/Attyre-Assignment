e16c30514ec12a3f08fa27d396455fcbd4bebf4f
Lipun <lipundani@gmail.com>
2024-10-14 18:48:32 +0530

    update: project updates leading to userdto update

--- src/main/java/com/example/Attyre/Assignment/DTO/UserDTO.java
+++ src/main/java/com/example/Attyre/Assignment/DTO/UserDTO.java
@@ -1,22 +1,28 @@
 package com.example.Attyre.Assignment.DTO;
 
+import com.example.Attyre.Assignment.Entity.Enums.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class UserDTO {
         @NotEmpty(message = "username cannot be empty or null")
-        @Length(min = 2, max = 10, message = "username should have more than 1 character and less than 11 characters")
+        @Length(min = 2, max = 15, message = "username should have more than 1 character and less than 11 characters")
         private String username;
 
-        @Length(min = 2, max = 10, message = "display_name should have more than 1 character and less than 11 characters")
-        private String display_name;
+        @Enumerated(EnumType.STRING)
+        private Gender gender;
 
-        private String profile_picture_url = null;
-        private String bio = null;
-        private int followers_count = 0;
-        private boolean verified = false;
+//        private List<> styles;
+//        private List<> brands;
+//        private List<> clothes;
+//        private int minPrice;
+//        private int maxPrice;
 }
