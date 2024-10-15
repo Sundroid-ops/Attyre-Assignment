e354467ad0034b64aed0d7afbce7c88adda7fd29
Lipun <lipundani@gmail.com>
2024-10-15 11:17:12 +0530

    jpa query findByUser to find preference data by userID

--- src/main/java/com/example/Attyre/Assignment/Repository/PreferenceRepo.java
+++ src/main/java/com/example/Attyre/Assignment/Repository/PreferenceRepo.java
@@ -2,8 +2,13 @@ package com.example.Attyre.Assignment.Repository;
 
 import com.example.Attyre.Assignment.Entity.Preference;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PreferenceRepo extends JpaRepository<Preference, Long> {
+    @Query("SELECT p FROM Preference p WHERE p.user.id = ?1")
+    Optional<Preference> findByUser(Long userID);
 }
