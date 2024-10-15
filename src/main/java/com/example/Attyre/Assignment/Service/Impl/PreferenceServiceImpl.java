fcf6b3ed56337f29dd5cbf1f51e1cfdc7c685d5f
Lipun <lipundani@gmail.com>
2024-10-15 11:17:46 +0530

    feat: getPreferenceByUserID added to find user preference added by user

--- src/main/java/com/example/Attyre/Assignment/Service/Impl/PreferenceServiceImpl.java
+++ src/main/java/com/example/Attyre/Assignment/Service/Impl/PreferenceServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.Attyre.Assignment.Service.Impl;
 import com.example.Attyre.Assignment.DTO.PreferenceDTO;
 import com.example.Attyre.Assignment.Entity.Preference;
 import com.example.Attyre.Assignment.Entity.User;
+import com.example.Attyre.Assignment.Exception.UserPreferenceNotFoundException;
 import com.example.Attyre.Assignment.Exception.UserPreferenceNotRegisteredException;
 import com.example.Attyre.Assignment.Repository.PreferenceRepo;
 import com.example.Attyre.Assignment.Service.PreferenceService;
@@ -11,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PreferenceServiceImpl implements PreferenceService {
@@ -23,6 +25,7 @@ public class PreferenceServiceImpl implements PreferenceService {
     private static final Logger logger = LoggerFactory.getLogger(PreferenceServiceImpl.class);
 
     @Override
+    @Transactional
     public void savePreferenceByUserID(PreferenceDTO preferenceDTO, Long userID) {
         if(preferenceDTO.getStyles() == null && preferenceDTO.getSeasons() == null && preferenceDTO.getBrands() == null && preferenceDTO.getCategory() == null)
             throw new UserPreferenceNotRegisteredException(String.valueOf(userID));
@@ -40,5 +43,15 @@ public class PreferenceServiceImpl implements PreferenceService {
 
         preferenceRepo.save(userPreference);
         logger.info("Preference of UserID : {} stored successfully", userID);
+    }
+
+    @Override
+    public Preference getPreferenceByUserID(Long userID) {
+        logger.info("sending recommendations based on user preference");
+        Preference userPreference = preferenceRepo.findByUser(userID)
+                .orElseThrow(() -> new UserPreferenceNotFoundException("User Preference Not Found for ID: "+ String.valueOf(userID)));
+
+        logger.info("user preference found for userID : {}", userPreference);
+        return userPreference;
     }
 }
--- src/main/java/com/example/Attyre/Assignment/Service/PreferenceService.java
+++ src/main/java/com/example/Attyre/Assignment/Service/PreferenceService.java
@@ -1,7 +1,14 @@
 package com.example.Attyre.Assignment.Service;
 
 import com.example.Attyre.Assignment.DTO.PreferenceDTO;
+import com.example.Attyre.Assignment.Entity.Preference;
+import com.example.Attyre.Assignment.Entity.Product;
+
+import java.util.List;
 
 public interface PreferenceService {
     public void savePreferenceByUserID(PreferenceDTO preferenceDTO, Long userID);
+
+    public Preference getPreferenceByUserID(Long userID);
+
 }
